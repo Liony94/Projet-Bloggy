14cccc674ebaa136d909ed11260378ab222a07c1
Liony <gui.prouchandy@gmail.com>
2023-03-12 22:32:30 +0100

    Dynamisation de mes question
    Ajout d'un clickable sur mes bloc question vers l'id question

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -50,6 +50,7 @@ class QuestionController extends AbstractController
             $comment->setCreatedAt(new \DateTimeImmutable());
             $comment->setRating(0);
             $comment->setQuestion($question);
+            $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
             $em->persist($comment);
             $em->flush();
             $this->addFlash('success', 'votre réponse a bien été prise en compte');
